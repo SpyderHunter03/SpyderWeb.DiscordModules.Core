aed81fd923d9ffaf11f089c9dda10c4ce1a145a1
Tristan Goetz <spyderhunter03@gmail.com>
2019-01-25 20:34:36 -0500

    Initial Commit

--- SpyderWeb.DiscordModules.Core/SpyderModuleBase.cs
+++ SpyderWeb.DiscordModules.Core/SpyderModuleBase.cs
@@ -2,26 +2,25 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using SpyderWeb.EmojiService;
 
 namespace SpyderWeb.DiscordModules.Core
 {
     public abstract class SpyderModuleBase : ModuleBase<SocketCommandContext>
     {
-        protected readonly IEmojiService _emojiService;
-
-        public SpyderModuleBase(IEmojiService emojiService)
-        {
-            _emojiService = emojiService;
+        private readonly IEmojiService _emojiService;
+        public SpyderModuleBase() {
+            _emojiService = new EmojiService.EmojiService();
         }
 
-        public Emoji TagNotFound => _emojiService.GetEmojiFromText("mag_right");
+         public Emoji TagNotFound => _emojiService.GetEmojiFromText("mag_right");
 
         public Emoji Pass => _emojiService.GetEmojiFromText("ok_hand");
-
+        
         public Emoji Fail => _emojiService.GetEmojiFromText("octagonal_sign");
-
+        
         public Emoji Removed => _emojiService.GetEmojiFromText("put_litter_in_its_place");
-
+        
         public Task ReactAsync(IEmote emote) => Context.Message.AddReactionAsync(emote);
     }
 }
